0ddae31e99a5dbfcce8f20f007d7d87e4b22dae2
NgocThuyen25 <thuyennguyen.241001@gmail.com>
2025-07-20 20:43:23 +0700

    update home page

---
+++ src/main/resources/static/js/home.js
@@ -0,0 +1,13 @@
+$('#film-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    items: 1,
+    navText: [
+        "<i class='fa-solid fa-hand-point-left'></i>",
+        "<i class='fa-solid fa-hand-point-right'></i>"
+    ]
+});
