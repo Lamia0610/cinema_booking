ae2f2287dce557b8d7c83654b34a7875c41f233d
NgocThuyen25 <thuyennguyen.241001@gmail.com>
2025-07-25 21:30:09 +0700

    update home page

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -11,3 +11,39 @@ $('#film-carousel').owlCarousel({
         "<i class='fa-solid fa-hand-point-right'></i>"
     ]
 });
+
+$('#movie-carousel').owlCarousel({
+  loop: true,
+  margin: 20,
+  center: true,
+  items: 3,
+  nav: true,
+  dots: false,
+  autoplay: true,
+  autoplayTimeout: 4000,
+});
+
+
+$('#promotion-carousel').owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  dots: false,
+  autoplay: true,
+  autoplayTimeout: 3000,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 2
+    },
+    1000: {
+      items: 4
+    }
+  },
+  navText: [
+    "<i class='fa-solid fa-chevron-left'></i>",
+    "<i class='fa-solid fa-chevron-right'></i>"
+  ]
+});
